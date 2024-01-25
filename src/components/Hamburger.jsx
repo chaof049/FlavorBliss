442faf589285fcea9f2153c46fc999335664531f
supreme <chaof049@gmail.com>
2024-01-25 10:18:03 +0545

    hamburger and language

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -1,176 +1,117 @@
 import { NavLink } from "react-router-dom";
 import { GoHomeFill } from "react-icons/go";
-// import { FaStar } from "react-icons/fa";
-// import { FaUsers } from "react-icons/fa";
-// import { IoMdPricetag } from "react-icons/io";
-// import { TbSquareLetterT } from "react-icons/tb";
-// import { MdKeyboardArrowDown } from "react-icons/md";
-// import { BsFillArrowRightSquareFill } from "react-icons/bs";
 import { useState } from "react";
-// import { RxHamburgerMenu } from "react-icons/rx";
+import { motion } from "framer-motion";
+import { BsArrowRightSquareFill } from "react-icons/bs";
+import { FaStar, FaUsers } from "react-icons/fa";
+import { IoMdArrowDropright, IoMdPricetag } from "react-icons/io";
+import { TbSquareLetterT } from "react-icons/tb";
 
 const Hamburger = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-  // const [showNavbar, setShowNavbar] = useState(false);
-
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     if (window.scrollY > 100) {
-  //       setShowNavbar(true);
-  //     } else {
-  //       setShowNavbar(false);
-  //     }
-  //   };
-
-  //   window.addEventListener("scroll", handleScroll);
-
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScroll);
-  //   };
-  // }, []);
   return (
-    <div className="absolute top-0 right-0 bg-white md:hidden">
-      <input
-        type="checkbox"
-        id="menu-toggle"
-        className="hidden"
-        checked={isOpen}
-        onChange={toggleMenu}
-      />
-      <label htmlFor="menu-toggle" className="cursor-pointer p-4">
-        {/* Open State */}
-        <img
-          src="/hamburger/hamburgerLine.png"
-          alt="Open Menu"
-          className={`w-10 h-10 menu-icon ${
-            isOpen ? "hidden" : ""
-          } p-2 bg-white rounded-xl`}
-        />
-        {/* Closed State */}
-        <img
-          src="/hamburger/hamburgerCross.png"
-          alt="Closed Menu"
-          className={`w-10 h-10 menu-icon ${
-            isOpen ? "" : "hidden"
-          } p-2 bg-white rounded-xl`}
-        />
-      </label>
-
-      <div
-        className={`menu ${
-          isOpen ? "block" : "hidden"
-        }  flex flex-col items-end`}
-      >
-        <div className="mb-4">
-          {/* Logo */}
-          <img src="/logo.png" alt="Logo" className="w-16 h-16" />
-        </div>
-        <ul>
-          <li className="mx-2 my-6 md:my-0">
-            <NavLink to="/" className="px-4 text-[#FE2F67] relative">
-              <span className="absolute -left-[1px] top-[3px]">
-                <GoHomeFill />
-              </span>
-              Homepage
-            </NavLink>
-          </li>
-        </ul>
+    <div className="absolute top-0 left-0 md:hidden w-full h-full">
+      <div className="w-full flex p-2 justify-end">
+        <button
+          className="border border-white bg-white bg-opacity-20 rounded-2xl p-2"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <img
+            src="/hamburger/hamburgerLine.png"
+            alt="Open Menu"
+            className={`w-9 h-9 menu-icon p-[10px] bg-white rounded-xl`}
+          />
+        </button>
       </div>
+
+      <motion.div
+        initial={{ x: "-100%" }}
+        animate={{ x: 0 }}
+        transition={{ duration: 0.8 }}
+        className={`menu absolute left-0 top-0 w-full h-full bg-white bg-opacity-80 backdrop-blur-[2px] z-20 ${
+          isOpen ? "block" : "hidden"
+        }  flex flex-col`}
+      >
+        <header className="flex justify-between p-2">
+          <img src="/logo.png" />
+          <button
+            className="border border-white bg-white bg-opacity-40 rounded-2xl p-2"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <img
+              src="/hamburger/hamburgerCross.png"
+              alt="Open Menu"
+              className={`w-9 h-9 menu-icon p-[10px] bg-white rounded-xl`}
+            />
+          </button>
+        </header>
+        <section className=" px-4 py-8">
+          <ul className="px-4 font-bold text-">
+            <li className="my-6 bg-white rounded-md w-full flex items-center py-2">
+              <NavLink
+                to="/"
+                className="px-4 text-[#FE2F67] flex items-center gap-4"
+              >
+                <GoHomeFill className="text-3xl" />
+                Homepage
+              </NavLink>
+            </li>
+
+            <li className=" my-6  rounded-md w-full flex items-center">
+              <NavLink
+                to="/features"
+                className="px-4 text-[#3A2A82] hover:text-[#FE2F67] w-full flex items-center justify-between"
+              >
+                <span className="flex items-center gap-4">
+                  <FaStar className="text-3xl" />
+                  Features
+                </span>
+                <IoMdArrowDropright className="text-2xl" />
+              </NavLink>
+            </li>
+
+            <li className="my-6  rounded-md w-full flex items-center">
+              <NavLink
+                to="/use-cases"
+                className="px-4 text-[#3A2A82] hover:text-[#FE2F67] w-full flex items-center justify-between"
+              >
+                <span className="flex items-center gap-4">
+                  <FaUsers className="text-3xl" />
+                  UseCases
+                </span>
+                <IoMdArrowDropright className="text-2xl" />
+              </NavLink>
+            </li>
+
+            <li className="my-6  rounded-md w-full flex items-center">
+              <NavLink
+                to="/pricing"
+                className="px-5 text-[#3A2A82] hover:text-[#FE2F67] flex items-center gap-4"
+              >
+                <IoMdPricetag className="text-3xl" />
+                Pricing
+              </NavLink>
+            </li>
+
+            <li className="my-6  rounded-md w-full flex items-center">
+              <NavLink
+                to="/blog"
+                className="px-5 text-[#3A2A82] hover:text-[#FE2F67] flex items-center gap-4"
+              >
+                <TbSquareLetterT className="text-3xl" />
+                Blog
+              </NavLink>
+            </li>
+          </ul>
+        </section>
+
+        <button className=" absolute bottom-10 left-4  w-[10rem] flex items-center justify-between px-3 py-3 rounded-xl bg-[#FE2F67] hover:bg-[#3A2A82] text-white font-bold shadow-lg shadow-[#fe2f669d] hover:shadow-[#3a2a82a3]">
+          Logout <BsArrowRightSquareFill className="text-xl rounded-md" />
+        </button>
+      </motion.div>
     </div>
   );
 };
 
 export default Hamburger;
-
-{
-  /* <nav
-  className={`z-[999] hidden md:flex md:items-center md:justify-between w-full top-0 bg-[#FCFCFE] backdrop-blur-md bg-opacity-85 px-32 py-2 items-center font-medium text-sm fixed duration-300 transition-all ${
-    showNavbar ? "transform translate-y-[0]" : "transform -translate-y-[100%]"
-  }`}
-  id="navbar"
->
-  <div className="border border-white w-fit bg-white bg-opacity-20 p-[6px] rounded-2xl flex justify-between items-center">
-    <img
-      src="/logo.png"
-      alt="logo"
-      className="bg-white bg-opacity-70 p-2 rounded-xl shadow-lg shadow-[rgba(254,47,102,0.49)] h-10"
-    ></img>
-  </div>
-  <ul className="md:flex md:items-center z-[-1] md:z-auto">
-    <li className="mx-2 my-6 md:my-0">
-      <NavLink to="/" className="px-4 text-[#FE2F67] relative">
-        <span className="absolute -left-[1px] top-[3px]">
-          <GoHomeFill />
-        </span>
-        Homepage
-      </NavLink>
-    </li>
-    <li className="mx-2 my-6 md:my-0">
-      <NavLink
-        to="/features"
-        className="px-4 text-[#3A2A82] hover:text-[#FE2F67] relative"
-      >
-        <span className="absolute -left-[1px] top-[3px]">
-          <FaStar />
-        </span>
-        <div className="absolute top-[250%] p-2 z-10 hidden  group-hover:block"></div>
-        Features
-        <span className="absolute top-[4px]">
-          <MdKeyboardArrowDown />
-        </span>
-      </NavLink>
-    </li>
-    <li className="mx-2 my-6 md:my-0">
-      <NavLink
-        to="/use-cases"
-        className="px-4 text-[#3A2A82] hover:text-[#FE2F67] relative"
-      >
-        <span className="absolute -left-[1px] top-[3px]">
-          <FaUsers />
-        </span>
-        UseCases
-        <span className="absolute top-[4px]">
-          <MdKeyboardArrowDown />
-        </span>
-      </NavLink>
-    </li>
-    <li className="mx-2 my-6 md:my-0">
-      <NavLink
-        to="/pricing"
-        className="px-5 text-[#3A2A82] hover:text-[#FE2F67] relative"
-      >
-        <span className="absolute -left-[1px] top-[3px]">
-          <IoMdPricetag />
-        </span>
-        Pricing
-      </NavLink>
-    </li>
-    <li className="mx-2 my-6 md:my-0">
-      <NavLink
-        to="/blog"
-        className="px-5 text-[#3A2A82] hover:text-[#FE2F67] relative"
-      >
-        <span className="absolute -left-[1px] top-[3px]">
-          <TbSquareLetterT />
-        </span>
-        Blog
-      </NavLink>
-    </li>
-  </ul>
-  <div className="flex">
-    <button className=" px-2 text-[#FE2F67]">Login</button>
-    <div className="bg-white bg-opacity-70 p-2 rounded-xl">
-      <button className=" px-3 py-1 pr-7 text-base text-white bg-[#FE2F67] hover:bg-[#3A2A82] rounded-xl relative shadow-lg shadow-[rgba(254,47,102,0.55)]">
-        Register
-        <span className="absolute right-2 top-[8px]">
-          <BsFillArrowRightSquareFill />
-        </span>
-      </button>
-    </div>
-  </div>
-</nav>; */
-}
--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 import { languageOptions } from "../assets/languageSelector";
+import { CiSearch } from "react-icons/ci";
 
 const Language = () => {
   const [selectedLanguage, setSelectedLanguage] = useState(languageOptions[0]);
@@ -17,20 +18,33 @@ const Language = () => {
       onMouseEnter={() => setDropdownVisible(true)}
       onMouseLeave={() => setDropdownVisible(false)}
     >
-      <div className="flex items-center justify-between p-3 bg-white group-hover:bg-white rounded-xl cursor-pointer">
+      <div
+        className={`flex items-center justify-between p-2 bg-white group-hover:bg-white rounded-lg ${
+          isDropdownVisible ? "rounded-b-none" : ""
+        } cursor-pointer`}
+      >
         <h1>{selectedLanguage.title}</h1>
         <img src={selectedLanguage.image} alt="flag" className="ml-2 w-4 h-4" />
       </div>
       {isDropdownVisible && (
-        <ul className="absolute w-full bg-white rounded">
+        <ul className="absolute w-full bg-white rounded-b-lg h-[200px] overflow-y-scroll overflow-x-hidden no-scrollbar p-1">
+          <li className="relative p-1">
+            <input
+              className="w-full p-[6px] border focus:outline-none focus:border-blue-400 rounded-md"
+              placeholder="Search"
+            />
+            <span className="absolute right-[6px] top-3 ml-[1px] text-[15px]">
+              <CiSearch />
+            </span>
+          </li>
           {languageOptions.map((value, index) => (
             <li
               key={index}
-              className="flex items-center justify-between p-3 cursor-pointer hover:bg-[#FF2F66]"
+              className="flex items-center justify-between p-[6px] cursor-pointer hover:bg-[#FF2F66] rounded-lg"
               onClick={() => handleLanguageSelect(value)}
             >
-              <h1 className="">{value.title}</h1>
-              <img src={value.image} alt="flag" className="ml-2 w-4 h-4" />
+              <h1 className="truncate">{value.title}</h1>
+              <img src={value.image} alt="flag" className="w-4 h-4" />
             </li>
           ))}
         </ul>
